ebf43d6ad656ecf6aa7d6e84b89de8a56cc9eed2
mMusashi <miy.musashi@protonmail.com>
2025-06-11 00:02:44 -0300

    agora está realmente tudo certo, se a vercel reclamar são fdp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import './globals.css'
-
 export default function Home() {
   return (
-    <div className="w-100vw text-kcire-w h-screen kcire-gradient">
-      <h1>Seu design é a primeira impressão. A gente faz ele ser inesquecível.</h1>
+    <div className="w-100vw text-kcire-w h-screen kcire-gradient bg-center bg-cover flex items-center justify-center">
+      <div className="flex flex-col items-center text-center text-kcire-w">
+        <div className="badge">
+          <h3>Branding forte,</h3>
+          <h3>resultado bruto!</h3>
+        </div>
+        <h1 className="">Seu design é a primeira impressão. A gente faz ele ser inesquecível.</h1>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './src/**/*.{ts,tsx,js,jsx,mdx}', // essencial no Next.js 13+ com app dir
-  ],
+// https://tailwindcss.com/docs/configuration
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx,js,jsx,mdx}'],
   theme: {
     extend: {
       colors: {
@@ -12,6 +10,7 @@ const config: Config = {
           2: 'hsla(289, 77%, 39%, 1)',
           3: 'hsla(274, 80%, 20%, 1)',
           4: 'hsla(274, 80%, 10%, 1)',
+          99: '#ff0000',
           w: 'hsla(0, 0%, 90%, 1)',
         },
       },
@@ -19,5 +18,3 @@ const config: Config = {
   },
   plugins: [],
 }
-
-export default config
